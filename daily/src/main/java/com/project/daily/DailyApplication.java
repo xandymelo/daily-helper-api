5882c0a4d1f2d4fd358326881a261c50739b6987
xandymelo <xandymelo@outlook.com.br>
2025-10-30 19:52:42 -0300

    first commit

---
+++ daily/src/main/java/com/project/daily/DailyApplication.java
@@ -0,0 +1,13 @@
+package com.project.daily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyApplication.class, args);
+	}
+
+}
